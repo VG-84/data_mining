8a20faf9d2c232dde47d7ed4f4de00eef0ad458c
vitalii_gavrilov <gavrilov.sbrf@yandex.ru>
2021-03-16 16:48:15 +0300

    lesson7

--- gb_parse/settings.py
+++ gb_parse/settings.py
@@ -1,4 +1,6 @@
 NEWSPIDER_MODULE = "gb_parse.spiders"
 
 LOG_ENABLE = True
-LOG_LEVEL = "INFO"
+LOG_LEVEL = "DEBUG"
+USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:85.0) Gecko/20100101 Firefox/85.0"
+ROBOTSTXT_OBEY = False
