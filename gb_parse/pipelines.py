141a4557b7af02c5f04a16b2331eacaa07ec7d41
vitalii_gavrilov <gavrilov.sbrf@yandex.ru>
2021-03-16 15:36:36 +0300

    lesson6

--- gb_parse/pipelines.py
+++ gb_parse/pipelines.py
@@ -1,17 +1,22 @@
 from itemadapter import ItemAdapter
+from scrapy import Request
+from scrapy.pipelines.images import ImagesPipeline
+
 from pymongo import MongoClient
 
 
-class GbParsePipeline:
+    def __init__(self):
     def process_item(self, item, spider):
+        self.db[spider.name].insert_one(item)
         return item
 
 
-class GbParseMongoPipeline:
-    def __init__(self):
-        client = MongoClient()
-        self.db = client["gb_parse"]
+class GbImageDownloadPipeline(ImagesPipeline):
+    def get_media_requests(self, item, info):
+        for url in item.get("photos", []):
+            yield Request(url)
 
-    def process_item(self, item, spider):
-        self.db[spider.name].insert_one(item)
+    def item_completed(self, results, item, info):
+        if item.get("photos"):
+            item["photos"] = [itm[1] for itm in results]
         return item
--- gb_parse/settings.py
+++ gb_parse/settings.py
@@ -1,6 +1,16 @@
-DOWNLOAD_DELAY = 1.2
+DOWNLOAD_DELAY = 0.05
+
+IMAGES_STORE = "images"
 
 ITEM_PIPELINES = {
-    "gb_parse.pipelines.GbParsePipeline": 300,
+    "gb_parse.pipelines.GbImageDownloadPipeline": 300,
     "gb_parse.pipelines.GbParseMongoPipeline": 400,
 }
+
+AUTOTHROTTLE_ENABLED = True
+
+AUTOTHROTTLE_START_DELAY = 2
+
+AUTOTHROTTLE_MAX_DELAY = 10
+
+AUTOTHROTTLE_DEBUG = True
