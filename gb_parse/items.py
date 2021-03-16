2727381ee0b8f37f412aa59a02744e08fe87d24c
vitalii_gavrilov <gavrilov.sbrf@yandex.ru>
2021-03-16 15:59:04 +0300

    dz06

--- gb_parse/items.py
+++ gb_parse/items.py
@@ -1,17 +1,19 @@
-import scrapy
-from itemloaders.processors import TakeFirst
 
-
-class GbParseItem(scrapy.Item):
-    pass
-
-
-class GbAutoYoulaItem(scrapy.Item):
-    _id = scrapy.Field()
-    url = scrapy.Field(output_processor=TakeFirst())
-    title = scrapy.Field(output_processor=TakeFirst())
-    price = scrapy.Field()
-    photos = scrapy.Field()
     characteristics = scrapy.Field()
     descriptions = scrapy.Field()
     author = scrapy.Field()
+
+
+class Insta(scrapy.Item):
+    _id = scrapy.Field()
+    date_parse = scrapy.Field()
+    data = scrapy.Field()
+    photos = scrapy.Field()
+
+
+class InstaTag(Insta):
+    pass
+
+
+class InstaPost(Insta):
+    pass
--- gb_parse/settings.py
+++ gb_parse/settings.py
@@ -1,16 +1,4 @@
-DOWNLOAD_DELAY = 0.05
+NEWSPIDER_MODULE = "gb_parse.spiders"
 
-IMAGES_STORE = "images"
-
-ITEM_PIPELINES = {
-    "gb_parse.pipelines.GbImageDownloadPipeline": 300,
-    "gb_parse.pipelines.GbParseMongoPipeline": 400,
-}
-
-AUTOTHROTTLE_ENABLED = True
-
-AUTOTHROTTLE_START_DELAY = 2
-
-AUTOTHROTTLE_MAX_DELAY = 10
-
-AUTOTHROTTLE_DEBUG = True
+LOG_ENABLE = True
+LOG_LEVEL = "INFO"
